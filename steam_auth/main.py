202e04c8bd9956d894d7b264d4f2e74123b76fb6
emopsych <hackydoom@yandex.ru>
2025-04-14 20:26:52 +0700

    Исправил файл main.py in steam_auth

--- steam_auth/main.py
+++ steam_auth/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
-from steam_auth.auth import start_steam_auth, handle_steam_response
+from auth import start_steam_auth, handle_steam_response
 
 app = FastAPI()
 
 @app.get("/auth/steam")
-async def auth_entry(tg_id: int):
+async def steam_entry(tg_id: int):
     return await start_steam_auth(tg_id)
 
 @app.get("/auth/steam/callback")
