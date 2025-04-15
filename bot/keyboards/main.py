2fd27d5c474d943f6202af2af4f161f3280f17de
emopsych <hackydoom@yandex.ru>
2025-04-15 14:00:04 +0700

    del

--- bot/keyboards/main.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-main_menu_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="🎮 Турниры")],
-    ],
-    resize_keyboard=True,
-    input_field_placeholder="Выберите раздел ↓"
-)
--- bot/states/steam.py
+++
@@ -1,4 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class SteamBinding(StatesGroup):
-    waiting_for_steam_id = State()
