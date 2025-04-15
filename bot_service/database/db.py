226495e374b30907da3d8612fabddad8a18b846a
emopsych <hackydoom@yandex.ru>
2025-04-15 20:39:18 +0700

    Main, start, profile

---
+++ bot_service/database/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import DATABASE_URL
+
+from typing import AsyncGenerator
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
