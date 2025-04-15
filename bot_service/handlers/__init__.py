8625437f7a986c549075ddf93555485a81b4a1a6
emopsych <hackydoom@yandex.ru>
2025-04-15 22:02:19 +0700

    tournament 1.0

--- bot_service/handlers/__init__.py
+++ bot_service/handlers/__init__.py
@@ -1,7 +1,8 @@
-from . import start, profile
+from . import start, profile, tournaments
 
 routers = [
     start.router,
     profile.router,
+    tournaments.router,
     # сюда добавим остальные
 ]
