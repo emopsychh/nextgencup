93d98355cf85f46506a929f2229753cc16e5ebc3
aaseewaa <161422176+aaseewaa@users.noreply.github.com>
2025-04-16 21:18:39 +0700

    webcam vers

--- web-service/src/components/BottomNav/BottomNav.js
+++ web-service/src/components/BottomNav/BottomNav.js
@@ -2,17 +2,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './BottomNav.css'; 
+import homeIcon from '../../asset/home.png' 
+import personIcon from '../../asset/person.png' 
+import contrIcon from '../../asset/controller.png'    
 function BottomNav() {
   return (
     <nav className="bottom-nav">
       <NavLink to="/" className="nav-item">
-        {/* Можно вставить иконку (SVG) или текст */}
+        <img src={homeIcon} alt="Главная" className="nav-icon" />
         <span>Главная</span>
       </NavLink>
       <NavLink to="/profile" className="nav-item">
+        <img src={personIcon} alt="Главная" className="nav-icon" />
         <span>Профиль</span>
       </NavLink>
       <NavLink to="/tournaments" className="nav-item">
+        <img src={contrIcon} alt="Главная" className="nav-icon" />
         <span>Турниры</span>
       </NavLink>
     </nav>
