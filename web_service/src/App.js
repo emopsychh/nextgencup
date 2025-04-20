0170a891879fd859a0133a2470ef1c6bb76722ca
Serj557 <Sergeyaaruchko@gmail.com>
2025-04-20 19:03:33 +0700

    Исправил стиль навигационного меню

--- web_service/src/App.js
+++ web_service/src/App.js
@@ -11,21 +11,19 @@ import Profile from './pages/Profile.js';
 import Tournaments from './pages/Tournaments.js';
 import BottomNav from './components/BottomNav/BottomNav.js';
 
-
 import './App.css'; 
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        {/* Место, где будет переключаться контент */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/tournaments" element={<Tournaments />} />
-        </Routes>
-
-        {/* Нижняя навигация */}
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/tournaments" element={<Tournaments />} />
+          </Routes>
+        </div>
         <BottomNav />
       </div>
     </Router>
