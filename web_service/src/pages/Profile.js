1ec2b7ebba5feb3f7ee52aa7bdc937ace3b4f886
aaseewaa <161422176+aaseewaa@users.noreply.github.com>
2025-04-21 15:31:23 +0700

    окно профиль

--- web_service/src/pages/Profile.js
+++ web_service/src/pages/Profile.js
@@ -1,13 +1,26 @@
 // src/pages/Profile.js
 import React from 'react';
-
-function Profile() {
+import './Profile.css';
+import settingsIcon from '../asset/settings.png';
+import defaultAvatar from '../asset/Sex.jpg';
+import arrowRight from '../asset/arrowRight.png';  
+export default function Profile() {
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <p>Это страница профиля</p>
+    <div className="profile-container">
+      <header className="profile-header">
+        <img src={settingsIcon} alt="Настройки" className="settings-icon" />
+        <img src={defaultAvatar} alt="Аватар" className="avatar" />
+        <h2 className="nickname">nickname</h2>
+      </header>
+
+      <div className="profile-menu">
+        {['Кошелёк', 'Достижения', 'Статистика'].map((label) => (
+          <div key={label} className="profile-menu-item">
+            <span className="menu-label">{label}</span>
+            <img src={arrowRight} alt="" className="menu-arrow" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default Profile;
